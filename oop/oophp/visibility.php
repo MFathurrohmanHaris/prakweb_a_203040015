56deb691e17b122167061f6b88b354c8f80c3f9c
MFathurrohmanHaris <harisfathurrohman142@gmail.com>
2022-10-06 06:42:44 +0700

    Menyelesaikan Video#9 Visibility (Access Modifier)

---
+++ oop/oophp/visibility.php
@@ -0,0 +1,121 @@
+<?php
+// Muhammad Fathurrohman Haris
+// 203040015
+/**
+ *  - Visibility
+ *    + Konsep yang digunakan untuk mengatur akses dari property dan method pada sebuah objek
+ *    + Ada 3 keyword visibility : public, protected, dan private
+ *    + public, dapat digunakan dimana saja, bahkan diluar kelas
+ *    + protected, hanya dapat digunakan didalam sebuah kelas beserta turunannya
+ *    + private, hanya dapat digunakan didalam sebuah kelas tertentu saja
+ * Kenapa menggunakan visibility ?
+ * Hanya untuk memperlihatkan aspek dari class yang dibutuhkan oleh 'client'
+ * Menentukan kebutuhan yang jelas untuk object
+ * memberikan kendali pada kode untuk menghindari 'bug'
+ */
+class Produk
+{
+  public $judul,
+    $penulis,
+    $penerbit;
+
+  protected $diskon = 0;
+
+  private $harga;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+  {
+    $this->judul = $judul;
+    $this->penulis = $penulis;
+    $this->penerbit = $penerbit;
+    $this->harga = $harga;
+  }
+
+  public function getLabel()
+  {
+    return "$this->penulis, $this->penerbit";
+  }
+
+
+  public function getInfoProduk()
+  {
+    $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+
+    return $str;
+  }
+
+  public function getHarga()
+  {
+    return $this->harga - ($this->harga * $this->diskon / 100);
+  }
+}
+
+class Komik extends Produk
+{
+  public $jmlHalaman;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0)
+  {
+    parent::__construct($judul, $penulis, $penerbit, $harga);
+
+    $this->jmlHalaman = $jmlHalaman;
+  }
+
+  public function getInfoProduk()
+  {
+    $str = "Komik : " . parent::getInfoProduk() . " - {$this->jmlHalaman} Halaman.";
+
+    return $str;
+  }
+}
+
+class Game extends Produk
+{
+  public $waktuMain;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuMain = 0)
+  {
+    parent::__construct($judul, $penulis, $penerbit, $harga);
+    $this->waktuMain = $waktuMain;
+  }
+
+  public function setDiskon($diskon)
+  {
+    $this->diskon = $diskon;
+  }
+
+  public function getInfoProduk()
+  {
+    $str = "Game : " . parent::getInfoProduk() . " - {$this->waktuMain} Jam.";
+
+    return $str;
+  }
+}
+
+class CetakInfoProduk
+{
+  public function cetak(Produk $produk)
+  {
+    $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+    return $str;
+  }
+}
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
+
+
+// Komik : Masashi Kishimoto, Shonen Jump
+// Game : Neil Druckmann, Sony Computer
+// Naruto | Masashi Kishimoto, Shonen Jump (Rp. 30000)
+
+// Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp. 30000) - 100 Halaman.
+// Game : Uncharted | Neil  Druckmann, Sony Computer (Rp. 250000) - 50 Jam.
+
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk2->getInfoProduk();
+echo "<hr>";
+
+$produk2->setDiskon(50);
+echo $produk2->getHarga();
